c527a5f404b9cadb5e5a4995664fd0b88d527ce7
lijingyun1111 <2211473895@qq.com>
2021-05-01 19:30:22 +0800

    Site updated: 2021-05-01 19:30:22

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,12 +3,12 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         $('[rel="icon"]').attr('href', "/imgs/favicon.ico");
          document.title = 'ヽ(●-`Д´-)ノ我生气了！';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         $('[rel="icon"]').attr('href', "/imgs/favicon.ico");
          document.title = 'ヾ(Ő∀Ő3)ノ你回来了！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
